32943f764e03143883b5796ca24b34b4e8401a30
cbogues <YOUR-EMAIL-ADDRESS>
2017-03-14 16:05:51 -0700

    header without styling

--- js/main.js
+++
@@ -1,3 +0,0 @@
-const lowers =['chris', 'holly', 'larry', 'audrey', 'ingrid'];
-const uppers = lowers.map(name => name.toUpperCase());
-uppers.forEach(name => console.log(`Hello, ${name}`));
---
+++ src/js/components/app.js
@@ -0,0 +1,13 @@
+import { div, addId } from '../builders';
+import navbar from './navbar';
+import hero from './hero';
+
+
+export default function app() {
+	const navbarEle = navbar();
+	const heroEle = hero();
+	const appEle = addId(div(navbarEle, heroEle), 'app-container');
+	
+	return appEle;
+
+}
